22fecdb77d814d69fdaeb9da4611359a89b4383e
jpmiller10 <joshua.philip.miller@gmail.com>
2020-04-03 14:37:15 -0600

    with README

--- README.md
+++ README.md
@@ -1,24 +1,66 @@
 # Politics Makes Strange Bedfellows
 In 2017, Emmanuel Macron and Marine Le Pen were the final two candidates in the French Presidential Election.  The two candidates had drastically different approaches to governing, and as such, the election was a major topic of discussion on Twitter.
 
-## The Data
-<a href="https://s3.us-east-2.amazonaws.com/jgartner-test-data/twitter/zippedData.zip">The data</a> you are provided a line delimited json file (746 MB) of tweets from France during that time period.  Your task is to leverage your understanding of Spark, visualization, and feature engineering to explore the dataset and provide answers to some basic questions.  
-
-# Your Task
-You and your team will have the task of reading in, cleaning, and exploring this dataset.  Your job is to gain insight into what is happening during the time period.  Your task for today is to produce the following:
-
-1. A python script containing helper functions.
-You should be working toward transforming this large cumbersome dataset into something that is regular and easily digestible.  You need to find inconsistencies in the data, and try to think about how you would clean them.  You can do cleaning in data as they are RDDs, DataFrames, or ideally both, but the processes should be calling function that are reusable.
-
-2. A presentation about your choices.
-Later this afternoon you'll stop work and get together as a class to present your findings.  You can either choose to use slides or jupyter notebooks.  The latter might be nice, because you may want to highlight bits of code.
+# Data Cleaning
+Our first step was stripping down this dataset to what we considered useful.
 
 
-## Hints
-### 1. Non-UTF Characters
-We suggest reading in the data into spark RDDs, not directly into Dataframes.  You can do so using the ```textFile``` command from the ```SparkContext```, and then getting python dictionaries using the ```json``` class.  If you do a ```take(1)```, it should work just fine.  If, however, you try to do a count, you'll end up throwing an error.  This happens because the ```json``` class fails when you encounter the non-utf8 characters in the dataset.  To get around this, you should wrap the json decoding in a ```try - except``` block, and return ```None``` if an exception is hit.  You can then filter out none.
+## Strip columns
+We kept the following columns:
+1. Tweet ID,
+1. Screen Name,
+1. Tweet Timestamp,
+1. Tweet Body,
+1. Location Name,
+1. Location - Country
+1. Hashtags
+1. Retweets
+1. Mentioned Users
 
-### 2. Messy Data
-This will be the most challenging dataset you've had to work with up to this point.  The data is somewhat large, and tweets are a complicated and messy source of information. Your first steps should be to understand which fields you'll be leveraging.  Once you've read in the data, start by doing a ```take(1)``` to get a feel for what a tweet JSON string looks like.
+<br>
 
-Also note that data is messy so you'll need to do a lot of checks and filter out inconsistent data.  <b>Being able to adjust based on error messages is an important skill, consider this a chance to practice!</b>
+## Strip Rows
+
+After limiting the number of fields we were working with, we worked to strip down the dataset to only tweets involving the French election.
+
+To do this, we chose some keywords. If the tweet didn't contain one of the keywords, we tossed it out.
+
+To start, we chose the following keywords:
+1. 'Le Pen'
+1. 'Macron'
+1. 'président'
+1. 'présidente'
+
+This keyword filtering brought out original dataset of **214,936** tweets down to **9,331**.
+
+![Image](images/filtered_counts.png)
+
+From these filtered tweets, we took the word frequency vector:
+```bash
++---------------+-----+                                                         
+|           word|count|
++---------------+-----+
+|         Macron| 1997|
+|@EmmanuelMacron| 1886|
+|             Le| 1583|
+|               | 1294|
+|              a| 1038|
+|              !| 1012|
+|            Pen|  913|
+|          c'est|  744|
+|        #Macron|  604|
+|          voter|  577|
+|              ?|  554|
+|              :|  510|
+|         MACRON|  494|
+|         Marine|  493|
+|           plus|  423|
+|             va|  419|
+|              -|  406|
+|          faire|  370|
+|         macron|  359|
+|           fait|  355|
++---------------+-----+
+```
+
+Given more time, we would filter whitespace and punctuation...
--- src/main.py
+++ src/main.py
@@ -1,33 +1,123 @@
 import pyspark as ps
+import pyspark.sql.functions as f  
+from datetime import datetime 
+from nltk.corpus import stopwords
+import nltk
 
-#keep = ['Marine Le Pen', 'Emmanuel Macron']
 
-#def 
 
+def filter_tweets(txt_lst, df, col):
+    '''
+    returns entries containing a word in txt_lst
+    '''
+    res = spark.createDataFrame(sc.emptyRDD(), df.schema)
+    for keyword in txt_lst:
+            filt_tweets = df.filter(f.lower(col).contains(keyword.lower()))
+            res = res.union(filt_tweets).distinct()
+    return res
+
+
+list_of_key_words = ['Le Pen', 'Macron', 'président', 'présidente']
+
+#This function will return a wordcount in the colname of the spark dataframe specified.
+def get_sparkdf_wordcount(spark_df, colname, stop_word_list):
+    res = spark_df.withColumn('word', f.explode(f.split(f.col(colname), ' ')))\
+    .groupBy('word')\
+    .count()\
+    .sort('count', ascending=False)
+    return res.filter(res.word.isin(stop_word_list)==False)
+    #Add returning histogram of word count?
+    
 spark = (ps.sql.SparkSession.builder 
         .master("local[4]") 
         .appName("Spark Case Study") 
         .getOrCreate()
         )
-sc = spark.sparkContext
 
+sc = spark.sparkContext
 
 french_tweets_df = spark.read.json('data/french_tweets.json')
 
-# french_tweets_df.printSchema()
-french_tweets_df_sample = french_tweets_df.sample(withReplacement=False, fraction=0.01, seed=42)
-french_tweets_df_sample2 = french_tweets_df_sample.sample(withReplacement=False, fraction=0.01, seed=44)
+french_tweets_df.createOrReplaceTempView("french_tweets_df_sample")
+
+cleaned_tweets = spark.sql('''
+                    SELECT
+                        id_str,
+                        user.screen_name,
+                        created_at,
+                        text,
+                        place.full_name,
+                        place.country_code
+                    FROM
+                        french_tweets_df_sample
+                    WHERE
+                        place.country_code = "FR"
+''')
+
+cand_tweets= spark.sql('''
+                    SELECT
+                        id_str,
+                        user.screen_name,
+                        created_at,
+                        text,
+                        place.full_name,
+                        place.country_code
+                    FROM
+                        french_tweets_df_sample
+                    WHERE
+                        user.screen_name = "EmmanuelMacron" OR user.screen_name = "MLP_officiel" 
+''')
 
 
-french_tweets_df_sample2.createOrReplaceTempView("french_tweets_df_sample")
-sample_tweets= spark.sql('''
-                    SELECT text
-                    FROM french_tweets_df_sample
-                    ''')
-sample_tweets.collect() 
+hashtag_df = spark.sql('''
+                    SELECT
+                        id_str,
+                        user.screen_name,
+                        hash_tags
+                    FROM
+                        french_tweets_df_sample
+                    LATERAL VIEW explode(entities.hashtags.text) myTable2 AS hash_tags
+''')
+cand_hashtag_df = spark.sql('''
+                    SELECT
+                        id_str,
+                        user.screen_name,
+                        hash_tags
+                    FROM
+                        french_tweets_df_sample
+                    LATERAL VIEW explode(entities.hashtags.text) myTable2 AS hash_tags
+                    WHERE
+                        user.screen_name = "EmmanuelMacron" OR user.screen_name = "MLP_officiel" 
+''')
 
-# schema = StructType([StructField('user_id', IntegerType(), True),
-#                         StructField('name', StringType(), True),
-#                         StructField('email', StringType(), True),
-#                         StructField('phone', StringType(), True)])
-# df_users = spark.read.csv('data/users.txt', schema = schema, header=None, sep =';')
+cand_retweet_df = spark.sql('''
+                    SELECT
+                        id_str,
+                        quoted_status.id_str,
+                        quoted_status.text,
+                        quoted_status.user.screen_name,
+                        quoted_status.retweet_count
+                    FROM
+                        french_tweets_df_sample
+                    WHERE
+                        quoted_status.user.screen_name = "EmmanuelMacron" OR quoted_status.user.screen_name = "MLP_officiel" 
+''')
+
+user_mentions_df = spark.sql('''
+                    SELECT
+                        id_str,
+                        user_mentions
+                    FROM
+                        french_tweets_df_sample
+                    LATERAL VIEW explode(entities.user_mentions.screen_name) myTable3 AS user_mentions
+''')
+
+# def word_cloud(data_frame, name):
+#     text = data_frame
+#     wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+#     plt.figure()
+#     plt.imshow(wordcloud, interpolation="bilinear")
+#     plt.axis("off")
+#     plt.show()
+
+
