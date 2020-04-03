d690a95599938eff7cab9d3357160aedc8e965b2
jpmiller10 <joshua.philip.miller@gmail.com>
2020-04-03 11:01:49 -0600

    commit 2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import pyspark as ps
 
+#keep = ['Marine Le Pen', 'Emmanuel Macron']
+
+#def 
+
 spark = (ps.sql.SparkSession.builder 
         .master("local[4]") 
-        .appName("sparkSQL exercise") 
+        .appName("Spark Case Study") 
         .getOrCreate()
         )
 sc = spark.sparkContext
@@ -10,7 +14,17 @@ sc = spark.sparkContext
 
 french_tweets_df = spark.read.json('data/french_tweets.json')
 
-french_tweets_df.printSchema()
+# french_tweets_df.printSchema()
+french_tweets_df_sample = french_tweets_df.sample(withReplacement=False, fraction=0.01, seed=42)
+french_tweets_df_sample2 = french_tweets_df_sample.sample(withReplacement=False, fraction=0.01, seed=44)
+
+
+french_tweets_df_sample2.createOrReplaceTempView("french_tweets_df_sample")
+sample_tweets= spark.sql('''
+                    SELECT text
+                    FROM french_tweets_df_sample
+                    ''')
+sample_tweets.collect() 
 
 # schema = StructType([StructField('user_id', IntegerType(), True),
 #                         StructField('name', StringType(), True),
